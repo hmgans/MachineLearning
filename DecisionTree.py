c1e19b1694ee0712d849d719709f3a953e55a092
Hank Gansert <hmgans@users.noreply.github.com>
2021-09-10 13:50:31 -0600

    Entropy

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,1 +1,13 @@
-print("heyo")
+# This file uses ML decision tree techiniques
+# Author: Hank Gansert
+# Date: 09/11/21
+import math
+
+
+
+def entropy(PropPos):
+    PropNeg = 1 - PropPos
+    return -PropPos * math.log2(PropPos) - PropNeg * math.log2(PropNeg)
+
+
+print(entropy(9/14))
