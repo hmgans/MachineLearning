3ece5e4039dea49b5e315603a15c1cf9dc4e40be
Hank Gansert <hmgans@users.noreply.github.com>
2021-09-14 10:44:53 -0600

    neww update

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,15 +2,31 @@
 # Author: Hank Gansert
 # Date: 09/11/21
 import math
+import pandas as pd
+from pandas.core.base import NoNewAttributesMixin
 
+df = pd.read_csv("TesterDT.csv", header=None)
+
+length = df.loc[0].size
+labels = []
+
+for i in range(length):
+    labels.append("col" + str(i))
+
+print(labels)
+df.columns = labels
+print(df.loc[df['col1'] > 0])
 
 
 def entropy(PropPos):
     PropNeg = 1 - PropPos
     return -PropPos * math.log2(PropPos) - PropNeg * math.log2(PropNeg)
 
+def giniIndex(PropPos):
+    PropNeg = 1 - PropPos
+    return 1 - (math.pow(PropPos,2) + math.pow(PropNeg,2)) 
 
+def decisionTree(filename, columnName, positiveIndicator):
+    return 0
 
-
-
-print(entropy(5/14))
+print( giniIndex(.25) )
